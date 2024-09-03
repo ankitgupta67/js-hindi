8ccada921586eacbd7d023b69d543bb3acdec914
Ankit Gupta <143529704+ankitgupta67@users.noreply.github.com>
2024-09-03 15:07:46 +0000

    about the const and let AND VAR

---
+++ 01b/01variable.js
@@ -0,0 +1,15 @@
+const accountId = 1452552
+let accountEmail= "ankiy.@gmail.com"
+var accountPassword="123456"
+accountcity="jaipur"
+let accountState
+
+
+// accountId=145255 const cannot be changed
+
+accountEmail="bsbudfhdu@gmail.com"
+accountPassword="ibdiui"
+accountcity="goa"
+
+console.table([accountId,accountEmail,accountPassword,accountcity,accountState])
+
