90a00a0c445a742d5f0fd972a01262e113deeb67
Ankit Gupta <143529704+ankitgupta67@users.noreply.github.com>
2024-09-04 13:20:07 +0000

    datatype

---
+++ 01b/datatype.js
@@ -0,0 +1,10 @@
+ "use strict";  //treat all the js code as a newer version
+
+//  alert(3+3)  //we are using the node not browser
+
+ let name= "ankit"
+ let age = 18
+ let isLogIn= true
+ let fo=10.20
+
+ console.log(typeof isLogIn)
