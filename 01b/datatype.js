18345d9ae386428b8170da0f807e97e95f9ea531
Ankit Gupta <143529704+ankitgupta67@users.noreply.github.com>
2024-09-04 13:23:10 +0000

    datatype in js

--- 01b/datatype.js
+++ 01b/datatype.js
@@ -7,4 +7,10 @@
  let isLogIn= true
  let fo=10.20
 
- console.log(typeof isLogIn)
+ console.log(typeof null)
+
+ //type of null is object kyu pta nahi ??
+
+ console.log(typeof undefined)
+
+ // type of undefined is unidefined 
