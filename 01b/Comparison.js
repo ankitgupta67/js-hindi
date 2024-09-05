667640f3a7e5f3ba962f235aab65419a1cf1493b
Ankit Gupta <143529704+ankitgupta67@users.noreply.github.com>
2024-09-05 13:49:32 +0000

    data and comparison

---
+++ 01b/Comparison.js
@@ -0,0 +1,28 @@
+console.log(2<1)
+console.log(2<=1)
+console.log(2>1)
+console.log(2>1)
+
+
+// console.log("2" > 1);
+// console.log("02" > 1);
+//compare the same data type value only
+
+
+
+
+
+// aise chejo se dur he rahe koi mtlb nhi h enka hmesa clean code likhe
+console.log(null > 0);
+console.log(null == 0);
+console.log(null >= 0);
+
+console.log(undefined == 0);
+console.log(undefined > 0);
+console.log(undefined < 0);
+
+// === 
+
+// data type bhi 
+
+console.log("2" === 2);
